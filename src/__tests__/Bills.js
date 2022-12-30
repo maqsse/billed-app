b1618642d786155141782abf8eefdf6e755bde63
max <32159805+maqsse@users.noreply.github.com>
2022-12-30 17:52:29 +0100

    Update Bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -36,7 +36,7 @@ const iconActivated = windowIcon.classList.contains('active-icon')
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.dataset.date)// changement a.innerHTML par a.dataset.date
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
